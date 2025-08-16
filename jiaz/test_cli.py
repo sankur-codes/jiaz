25736321f07a54f89314101abecbb1e160cc880e
shaheen sayyed <shsayyed@redhat.com>
2025-08-16 14:16:34 +0530

    revert the issue test

--- jiaz/test_cli.py
+++ jiaz/test_cli.py
@@ -3,14 +3,13 @@
 from unittest.mock import patch
 
 import pytest
-from typer.testing import CliRunner
-
 from jiaz.cli import app, main
+from typer.testing import CliRunner
 
 
 @pytest.fixture
 def runner():
-    return CliRunner()     
+    return CliRunner()
 
 
 class TestCLI:
